ac9ca6b12d260928caffc4e1fe104ab4bd1cb07a
Joris Meys <Joris.Meys@UGent.be>
2018-03-20 18:23:53 +0100

    added unit tests

--- pkg/xnet/tests/testthat/test_calculations.R
+++ pkg/xnet/tests/testthat/test_calculations.R
@@ -1,13 +1,27 @@
 library(xnet)
-context("Matrix calculations")
+context("matrix calculations for weights and predictions")
 
-X <- matrix(c(1,2,3,4,2,1,4,3,5,2,1,3,3,1,2,4),ncol = 4)
-Xeig <- eigen(X)
-mat <- Xeig$vectors
-vec <- Xeig$values
+K <- matrix(c(1,2,3,4,2,1,4,3,5,2,1,3,3,1,2,4),ncol = 4)
+G <- matrix(c(5,1,1,6,5,4,2,4,1,1,3,1,2,4,3,6,3,3,1,2,6,1,4,4,6),ncol = 5)
+Y <- matrix(c(1,0,0,1,0,0,1,1,0,0,0,0,0,1,0,0,1,0,1,1), ncol = 5)
+
+Keig <- eigen(X)
+Kmat <- Keig$vectors
+Kvec <- Keig$values
+
+Geig <- eigen(G)
+Gmat <- Geig$vectors
+Gvec <- Geig$values
+
 lambda <- 0.001
-H <- mat %*% diag(vec) %*% solve(diag(vec) + lambda*diag(4)) %*% t(mat)
+H <- Kmat %*% diag(Kvec) %*% solve(diag(Kvec) + lambda*diag(4)) %*% t(Kmat)
+W <- Kmat %*% solve(diag(Kvec) + lambda*diag(4)) %*% t(Kmat)
 
-test_that("hat matrix is calculated correctly",{
-  expect_equal(H, eigen2hat(mat, vec, lambda))
+test_that("hat and map matrix is calculated correctly",{
+  expect_equal(H, eigen2hat(Kmat, Kvec, lambda))
+  expect_equal(W, eigen2map(Kmat, Kvec, lambda))
+})
+
+test_that("Model is fitted correctly",{
+  expect_equal(1,2)
 })
--- pkg/xnet/tests/testthat/test_shortcuts.R
+++ pkg/xnet/tests/testthat/test_shortcuts.R
@@ -1,7 +1,29 @@
 library(xnet)
 context("LOO shortcuts")
+data(drugtarget)
+
+test_that("get_loo_fun returns the correct function",{
+
+  expect_equal(get_loo_fun('interaction', FALSE, replaceby0 = FALSE), loo.i)
+  expect_equal(get_loo_fun('interaction', FALSE, replaceby0 = TRUE), loo.i0)
+  expect_equal(get_loo_fun('row', FALSE), loo.r)
+  expect_equal(get_loo_fun('column',FALSE), loo.c)
+  expect_equal(get_loo_fun('both',FALSE), loo.b)
+
+  expect_equal(get_loo_fun('interaction', TRUE,
+                           symmetry = "skewed",
+                           replaceby0 = FALSE), loo.e.skew)
+  expect_equal(get_loo_fun('interaction', TRUE,
+                           symmetry = "symmetric",
+                           replaceby0 = FALSE), loo.e.sym)
+  expect_equal(get_loo_fun('row', TRUE), loo.v)
+  expect_equal(get_loo_fun('column', TRUE), loo.v)
+  expect_equal(get_loo_fun('both', TRUE), loo.v)
+  expect_error(get_loo_fun('column', FALSE, replaceby0 = TRUE))
+})
 
 test_that("shortcut I bipartite works",{
 
   expect_equal(1,1)
 })
+

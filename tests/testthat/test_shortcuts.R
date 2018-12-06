de6e7baadcc6fa02e41d81e0a00eeaadb5f6b330
Joris Meys <Joris.Meys@UGent.be>
2018-12-06 13:13:18 +0100

    Update the tests for shortcuts to include those for homogenous networks.

--- tests/testthat/test_shortcuts.R
+++ tests/testthat/test_shortcuts.R
@@ -1,8 +1,6 @@
 context("LOO shortcuts")
 
-# Setup for testing
-
-## Heterogenous network
+# Preparation -----------------------------------------------------
 dfile <- system.file("testdata","testdata.rda", package = "xnet")
 
 load(dfile)
@@ -15,7 +13,7 @@ Hk <- hat(mod, 'row')
 Hg <- hat(mod, 'column')
 pred <- fitted(mod)
 
-## Homogenous network
+# symmetric network
 dfile <- system.file("testdata","testdataH.rda", package = "xnet")
 load(dfile)
 modh <- tskrr(Yh, Kh, lambda = lambdak)
@@ -28,6 +26,11 @@ mods <- tskrr(Ys, Kh, lambda = lambdak)
 eigKs <- get_eigen(mods)
 Hks <- hat(mods)
 preds <- fitted(mods)
+
+# Tests -----------------------------------------------------------
+
+## get_loo_fun ----------------------------------------------------
+
 
 test_that("get_loo_fun returns the correct function",{
 
@@ -48,13 +51,15 @@ test_that("get_loo_fun returns the correct function",{
 })
 
 predict_ij <- function(Y,Hk, Hg, i, j){
-  Hk[i,] %*% Y %*% Hg[,j]
+  Hk[i,, drop = FALSE] %*% Y %*% Hg[,j, drop = FALSE]
 }
+
+## Heterogenous network -------------------------------------------
 
 test_that("shortcuts bipartite networks work",{
 
   # Setting I
-  looI <- xnet:::loo(mod)
+  looI <- loo(mod)
   looItest <- sapply(seq_len(ncol(Y)),
                      function(y) sapply(seq_len(nrow(Y)),
                                         function(x){Ytilde <- Y
@@ -87,5 +92,69 @@ test_that("shortcuts bipartite networks work",{
   # sapply gives the matrix in a different way.
   expect_equal(looR, t(looRtest))
 
+  # Setting Col
+  looC <- loo(mod, exclusion = "column")
+  looCtest <- sapply(seq_len(ncol(Y)), function(j){
+    modx <- tskrr(Y[, -j], K, G[-j, -j], lambda = c(lambdak,lambdag))
+    predict(modx, K, G[-j, j])
+  })
+
+  expect_equal(looC, looCtest)
+
+  # Setting both
+  i <- 3
+  j <- 4
+  looB <- loo(mod, exclusion = "both")[i,j, drop = FALSE]
+  modx <- tskrr(Y[-i, -j], K[-i,-i], G[-j,-j], lambda = c(lambdak,lambdag))
+  looBtest <- predict(modx, K[i, -i], G[-j, j])
+  expect_equal(looB, looBtest)
+
 })
 
+## Homogenous network ---------------------------------------------
+
+test_that("shortcuts homogenous networks work", {
+
+  # Shortcut for the moment is not tested on the diagonal, as that
+  # gives very weird results. Replacement by looE result is maybe
+  # not the best way forward.
+  looE <- loo(modh)
+  looEtest <- sapply(seq_len(ncol(Yh)),
+                     function(y) sapply(seq_len(nrow(Yh)),
+                                        function(x){Ytilde <- Yh
+                                        Ytilde[x,y] <- looE[x,y]
+                                        Ytilde[y,x] <- looE[x,y]
+                                        if(x == y) Ytilde[x,y] else
+                                        predict_ij(Ytilde, Hkh, Hkh, x, y) }
+                     )
+  )
+
+
+  expect_equal(looE,looEtest)
+
+  # setting E0
+  looE0 <- loo(modh, replaceby0 = TRUE)
+  looE0test <- sapply(seq_len(ncol(Yh)),
+                     function(y) sapply(seq_len(nrow(Yh)),
+                                        function(x){Ytilde <- Yh
+                                        Ytilde[x,y] <- 0
+                                        Ytilde[y,x] <- 0
+                                        predict_ij(Ytilde, Hkh, Hkh, x, y)
+                                        }
+                     )
+  )
+
+  expect_equal(looE0, looE0test)
+
+  # Setting both
+
+  looV <- loo(modh, exclusion = "both")
+  looVtest <- sapply(seq_len(nrow(Yh)), function(i){
+
+    modx <- tskrr(Yh[-i, -i], Kh[-i, -i], lambda = lambdak)
+    predict(modx, Kh[i, -i, drop = FALSE], Kh[-i,i,drop = FALSE])
+  })
+
+  expect_equal(looV, t(looVtest))
+
+})

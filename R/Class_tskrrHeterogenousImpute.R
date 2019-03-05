6dd1fe382b51f9f74211a2d3033435f44ec7cfaf
Joris Meys <Joris.Meys@UGent.be>
2019-03-05 19:37:20 +0100

    start impute machinery. NEEDS WORK!

---
+++ R/Class_tskrrHeterogenousImpute.R
@@ -0,0 +1,38 @@
+#' Class tskrrHeterogenousImpute
+#'
+#' The class \code{tskrrHeterogenousImpute} is a subclass of the
+#' class \code{\link[xnet:tskrrHeterogenous-class]{tskrrHeterogenous}}
+#' specifically for heterogenous networks with imputed values. It is
+#' the result of the function \code{\link{impute_tskrr}}.
+#'
+#' @slot y the matrix with responses
+#' @slot k the eigen decomposition of the kernel matrix for the rows
+#' @slot lambda.k the lambda value used for k
+#' @slot pred the matrix with the predictions
+#' @slot g the eigen decomposition of the kernel matrix for the columns
+#' @slot lambda.g the lambda value used for g
+#' @slot has.hat a logical value indicating whether the kernel hat matrices
+#' are stored in the object.
+#' @slot Hk the kernel hat matrix for the rows.
+#' @slot Hg the kernel hat matrix for the columns.
+#' @slot labels a list with elements \code{k} and \code{g} (see
+#' \code{\link{tskrr-class}}).
+#'  If any element is \code{NA}, the labels used
+#' are integers indicating the row resp column number.
+#' @slot imputeid a vector with integer values indicating which of
+#' the values in \code{y} are imputed
+#' @slot imputemethod a character value with either \code{"predictions"} or
+#' "loo" indicating whether the predictions or the leave-one-out
+#' crossvalidation values were used in the imputation.
+#' @slot loofun if imputemethod is "loo", this slot contains the loo
+#' function used during imputation.
+#'
+#' @include Class_tskrrHeterogenous.R
+#' @rdname tskrrHeterogenousImpute-class
+#' @aliases tskrrHeterogenousImpute
+#' @exportClass tskrrHeterogenousImpute
+setClass("tskrrHeterogenousImpute",
+         contains = "tskrrHeterogenous",
+         slots = c(imputeid = "integer",
+                   imputemethod = "character",
+                   loofun = "function"))

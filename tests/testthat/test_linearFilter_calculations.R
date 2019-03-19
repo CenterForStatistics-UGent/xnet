0889b81b9fb1bdbc8a8d6a6d8c6eb77e911143c2
Joris Meys <Joris.Meys@UGent.be>
2019-03-19 16:36:06 +0100

    Constrained alphas to sum to 1

--- tests/testthat/test_linearFilter_calculations.R
+++ tests/testthat/test_linearFilter_calculations.R
@@ -56,3 +56,10 @@ test_that("NAs are dealt with properly", {
   expect_identical(alpha(linFNONA), alphas)
   expect_equal(fitted(linFNONA),preds)
 })
+
+test_that("linear_filter returns correct errors",{
+  expect_error(linear_filter(Y, alpha = c(0.5,0.5)),
+               "alpha should .* either 1 or 4 values")
+  expect_error(linear_filter(Y, alpha = c(0.1,0.1,0.1,0.1)),
+               "alpha values should add up to 1")
+})

c67f1ee26e228e058a80d58a1576313b8d5f7b8e
Joris Meys <Joris.Meys@UGent.be>
2019-03-20 18:42:36 +0100

    Added tune method for tskrrTune (so it can be retuned if necessary)

--- tests/testthat/test_heterogenous_tune.R
+++ tests/testthat/test_heterogenous_tune.R
@@ -8,6 +8,8 @@ load(dfile)
 lambdas <- c(0.01,0.015)
 mod <- tskrr(Y,K,G,lambda = lambdas)
 
+# Test errors ---------------------------------------------
+
 test_that("input of tune is correctly processed",{
   expect_error(tune(mod, lim = "a"),
                "lim .* numeric vector .* list with two")
@@ -15,4 +17,18 @@ test_that("input of tune is correctly processed",{
                "lim .* 2 numeric values")
   expect_error(tune(mod, lim = list(c(0.01,1), c(1,2,3))),
                "lim .* 2 numeric values")
+})
+
+# Test output ---------------------------------------------
+tuned <- tune(mod,
+              lim = list(c(0.001,1),c(0.015,2)),
+              ngrid = list(10,20),
+              exclusion = "row")
+
+test_that("Output of tuned model is correct", {
+  expect_identical(tuned,
+                   tune(tuned,
+                        lim = list(c(0.001,1),c(0.015,2)),
+                        ngrid = list(10,20),
+                        exclusion = "row"))
 })

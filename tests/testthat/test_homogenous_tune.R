f6dd7fd84c492ba96f8b8bacbaebbfdc58d62483
Joris Meys <Joris.Meys@UGent.be>
2019-03-21 14:49:33 +0100

    Add test homogenous tune

---
+++ tests/testthat/test_homogenous_tune.R
@@ -0,0 +1,56 @@
+context("tuning homogenous models")
+
+dfile <- system.file("testdata","testdataH.rda", package = "xnet")
+
+load(dfile)
+
+# Create test model
+lambdas <- c(0.01)
+mod <- tskrr(Yh,Kh,lambda = lambdas)
+
+# Test errors ---------------------------------------------
+
+test_that("input of tune is correctly processed",{
+  expect_error(tune(mod, lim = "a"),
+               "lim .* single series of numeric values")
+  expect_error(tune(mod, lim = numeric(0)),
+               "lim needs 2 numeric values")
+  expect_error(tune(mod, lim = list(c(0.01,1), c(1,2))),
+               "lim .* single series of numeric values")
+  expect_error(tune(mod, ngrid = list(12,12)),
+               "ngrid .* single series of numeric values")
+
+})
+
+# Test output ---------------------------------------------
+tuned <- tune(mod,
+              lim = list(c(0.001,1)),
+              ngrid = list(20),
+              exclusion = "both")
+
+test_that("Output of tuned model is correct", {
+  # retuning should give the exact same outcome
+  expect_identical(tuned,
+                   tune(tuned,
+                        lim = list(c(0.001,1)),
+                        ngrid = list(20),
+                        exclusion = "both"))
+  # You should get the exact same loo function
+  expect_identical(get_loo_fun(tuned),
+                   get_loo_fun(mod,
+                               exclusion = "both"))
+  # Loss function should be correct
+  expect_identical(tuned@loss_function,
+                   loss_mse)
+
+})
+
+# Test behaviour as tskrr ---------------------------------
+test_that("get_loo_fun works correctly on tuned homogenous models",{
+  expect_identical(get_loo_fun(tuned,
+                               exclusion = "interaction",
+                               replaceby0 = TRUE),
+                   get_loo_fun(mod,
+                               exclusion = "interaction",
+                               replaceby0 = TRUE))
+})
